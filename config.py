af057edaeb229bca42f6524ae9a7e070b3f26af3
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-02-26 13:18:17 +0000

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ training_config = {
     'n_layers': 4,           # Number of layers in the model
     'num_heads': 8,          # Number of attention heads
     'dropout': 0.1,          # Dropout rate | Consider, if being used as compression. Do we actually WANT to overfit?
-    'batch_size': 8,      # Batch size ## REDUCE THIS IF < 24GB GPU
-    'micro_batch_size': 256, # Micro batch size # If you increase this you need to reduce the batch size
+    'batch_size': 8,      
+    'micro_batch_size': 256, # Micro batch size, this is the height of the image
     'learning_rate': 1e-4,   # Learning rate
     'weight_decay': 1e-5,    # Weight decay for the optimizer
     'n_epochs': 50,          # Number of training epochs
-    'output_dim': 768,             
+    'output_dim': 768,           # this is width x 3 for RGB  
     'delta': 1,              
     'w1': 1.0,               
     'w2': 1.0, 
@@ -18,8 +18,8 @@ training_config = {
     'use_multi_gpu' : False,   
     'num_gpus' : 1,               
     'warmup_epochs': 0, 
-    'input_dim': 768,  
-    'frame_size': 256,
+    'input_dim': 768,    # this is width x 3 for RGB  - we stretch the 64x64 image up to create pixillated version of same size as taret
+    'frame_size': 256, # this is the height of the image
     'root_dir': r"dataset/data",     
     'model_path': r"out/model.pth",
     'checkpoint_path': r"out/checkpoints/checkpoint.pth", 
